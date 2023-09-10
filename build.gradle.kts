5063e8db35d73fd1fa555c9b7e40cc9a7fdd74b8
Muhammad Moin Raza Khan <muhammadmoin74@gmail.com>
2023-09-10 23:15:47 +0500

    updated agp,kotlin and compose version into a single file.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     //trick: for the same plugin versions in all sub-modules
-    id("com.android.application").version("8.0.2").apply(false)
-    id("com.android.library").version("8.0.2").apply(false)
-    kotlin("android").version("1.9.10").apply(false)
-    kotlin("multiplatform").version("1.9.10").apply(false)
-    id("org.jetbrains.compose").version("1.5.1") apply false
+    id("com.android.application").apply(false)
+    id("com.android.library").apply(false)
+    kotlin("android").apply(false)
+    kotlin("multiplatform").apply(false)
+    id("org.jetbrains.compose").apply(false)
 }
 
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,20 @@ pluginManagement {
         gradlePluginPortal()
         mavenCentral()
     }
+
+    plugins {
+        val agpVersion = extra["agp.version"] as String
+        val kotlinVersion = extra["kotlin.version"] as String
+        val composeVersion = extra["compose.version"] as String
+
+        id("com.android.application").version(agpVersion).apply(false)
+        id("com.android.library").version(agpVersion).apply(false)
+        kotlin("android").version(kotlinVersion).apply(false)
+        kotlin("multiplatform").version(kotlinVersion).apply(false)
+        id("org.jetbrains.compose").version(composeVersion) apply false
+
+
+    }
 }
 
 dependencyResolutionManagement {
