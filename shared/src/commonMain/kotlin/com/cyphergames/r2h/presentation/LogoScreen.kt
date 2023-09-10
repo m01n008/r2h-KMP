e66cadc0af48957f81493e08d3248a424c637f94
Muhammad Moin Raza Khan <muhammadmoin74@gmail.com>
2023-09-10 21:42:25 +0500

    Consumed palette colors in LogoScreen.kt

--- shared/src/commonMain/kotlin/com/cyphergames/r2h/presentation/LogoScreen.kt
+++ shared/src/commonMain/kotlin/com/cyphergames/r2h/presentation/LogoScreen.kt
@@ -9,14 +9,14 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.cyphergames.r2h.style.Palette
 
 @Composable
 fun LogoScreen(){
     Surface(modifier = Modifier.fillMaxSize(),
-            color = Color(0,120,0)) {
+            color = Palette.PakistanGreen) {
 
         Column (
             verticalArrangement = Arrangement.Center,
@@ -26,11 +26,11 @@ fun LogoScreen(){
                 text = "r2h",
                 fontWeight = FontWeight.ExtraBold,
                 fontSize = 80.sp,
-                color = Color.White
+                color = Palette.White
             )
 
             LinearProgressIndicator(
-                color = Color.Black
+                color = Palette.Black
 
 
             )
