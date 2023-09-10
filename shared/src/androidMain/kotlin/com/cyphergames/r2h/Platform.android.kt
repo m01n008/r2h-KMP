074d29e6aa6d36c1ad9f99603770fd33001ec9d2
Muhammad Moin Raza Khan <muhammadmoin74@gmail.com>
2023-09-10 21:34:19 +0500

    deleted un-necessary files

--- shared/src/androidMain/kotlin/com/cyphergames/r2h/Platform.android.kt
+++
@@ -1,7 +0,0 @@
-package com.cyphergames.r2h
-
-class AndroidPlatform : Platform {
-    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
